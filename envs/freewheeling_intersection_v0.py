6f32b1fc3e7e135119334d2c5cc046c9a55f55c7
Metro <3170101740@zju.edu.cn>
2021-11-17 11:37:09 +0800

    update 11/17 complete reset in env and start step

--- envs/freewheeling_intersection_v0.py
+++ envs/freewheeling_intersection_v0.py
@@ -84,7 +84,7 @@ class Freewheeling_Intersection_V0(gym.Env):
         """
         Connect with the sumo instance, could be multiprocess.
 
-        :return:
+        :return: dic, speed and position of different vehicle types
         """
         # sumoBinary = "/path/to/sumo"
         sumoBinary = 'D:/SUMO/bin/sumo'
@@ -92,7 +92,67 @@ class Freewheeling_Intersection_V0(gym.Env):
 
         # create instances
         traci.start(sumoCmd, label='sim1')
-        traci.domain.
+
+        # dic to save vehicles' speed and position info w.r.t its vehicle type
+        # e.g. vehicles_speed = {'NW_right':'vehicle_id_0', 'vehicle_id_6',
+        #                        'NS_through':...}
+        vehicles_speed = {}
+        vehicles_position = {}
+
+        # the edgeID is defined in FW_Inter.edg.xml
+        # as you may have different definition in your own .edg.xml, change it.
+        edgeIDs = ['north_in', 'east_in', 'south_in', 'west_in']
+
+        # vehicle_types will help to filter the vehicles on the same edge but have different direction.
+        vehicle_types = ['NW_right', 'NS_through', 'NE_left',
+                         'EN_right', 'EW_through', 'ES_left',
+                         'SE_right', 'SN_through', 'SW_left',
+                         'WS_right', 'WE_through', 'WN_left']
+
+        # a cursor to indicate which vehicle type is being selected
+        type_cursor = 0
+
+        for edgeID in edgeIDs:
+            traci.edge.subscribeContext(edgeID, tc.CMD_GET_VEHICLETYPE_VARIABLE,
+                                        [tc.LAST_STEP_VEHICLE_ID_LIST])
+            vehicles_on_specific_edge = traci.edge.getContextSubscriptionResults(edgeID)
+            for i in range(3):  # on one specific edge, there are three different vehicle types.
+                for vehicleID in vehicles_on_specific_edge:
+                    traci.vehicle.subscribeContext(vehicleID, tc.CMD_GET_VEHICLETYPE_VARIABLE,
+                                                   [tc.VAR_SPEED, tc.VAR_POSITION])
+                    traci.vehicle.addSubscriptionFilterVType(vehicle_types[type_cursor])
+                    vehicles_speed[vehicle_types[type_cursor]] = \
+                        traci.vehicle.getContextSubscriptionResults(vehicleID)
+                    vehicles_position[vehicle_types[type_cursor]] = \
+                        traci.vehicle.getContextSubscriptionResults(vehicleID)
+                type_cursor += 1
+
+        return vehicles_speed, vehicles_position
+
+    def step(self, action):
+        """
+
+        Note: the sumo(or traci) doesn't need an action every step until one specific phase is over,
+              but the abstract method 'step' needs as you can see.
+              Thus only a new action is input  will we change the traffic light state, otherwise just do
+              traci.simulationStep()
+
+
+        :param action: array [1 * 2], the former is the phase next period, and the latter is its duration respectively.
+        :return: next_state, reward, done, _
+        """
+
+        assert action is np.array , 'wrong data structure for action'
+        phase_next = action[0]
+        phase_duration = action[1]
+
+
+
+
+
+
+
+
 
 
 
--- test.py
+++ test.py
@@ -10,4 +10,38 @@ sumoCmd = [sumoBinary, '-c', 'FW_Inter.sumocfg']
 
 # create instances
 traci.start(sumoCmd, label='sim1')
-traci.
+
+# dic to save vehicles' speed and position info w.r.t its vehicle type
+# e.g. vehicles_speed = {'NW_right':'vehicle_id_0', 'vehicle_id_6',
+#                        'NS_through':...}
+vehicles_speed = {}
+vehicles_position = {}
+
+# the edgeID is defined in FW_Inter.edg.xml
+# as you may have different definition in your own .edg.xml, change it.
+edgeIDs = ['north_in', 'east_in', 'south_in', 'west_in']
+
+# vehicle_types will help to filter the vehicles on the same edge but have different direction.
+vehicle_types = ['NW_right', 'NS_through', 'NE_left',
+                 'EN_right', 'EW_through', 'ES_left',
+                 'SE_right', 'SN_through', 'SW_left',
+                 'WS_right', 'WE_through', 'WN_left']
+
+# a cursor to indicate which vehicle type is being selected
+type_cursor = 0
+
+for edgeID in edgeIDs:
+    traci.edge.subscribeContext(edgeID, tc.CMD_GET_VEHICLETYPE_VARIABLE,
+                                [tc.LAST_STEP_VEHICLE_ID_LIST])
+    vehicles_on_specific_edge = traci.edge.getContextSubscriptionResults(edgeID)
+    for i in range(3):  # on one specific edge, there are three different vehicle types.
+        for vehicleID in vehicles_on_specific_edge:
+            traci.vehicle.subscribeContext(vehicleID, tc.CMD_GET_VEHICLETYPE_VARIABLE,
+                                           [tc.VAR_SPEED, tc.VAR_POSITION])
+            traci.vehicle.addSubscriptionFilterVType(vehicle_types[type_cursor])
+            vehicles_speed[vehicle_types[type_cursor]] = \
+                traci.vehicle.getContextSubscriptionResults(vehicleID)
+            vehicles_position[vehicle_types[type_cursor]] = \
+                traci.vehicle.getContextSubscriptionResults(vehicleID)
+        type_cursor += 1
+
